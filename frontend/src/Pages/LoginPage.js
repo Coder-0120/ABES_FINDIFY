d4e2be34c223aa1ada0ed56118085d6869c86a3d
Anshul Verma <av4862187@gmail.com>
2025-06-18 23:56:20 +0530

    fix: removed unused Navigate imports to fix vercel build

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../Styles/Login.css';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import Navbar from '../Components/Navbar';
 import axios from 'axios';
