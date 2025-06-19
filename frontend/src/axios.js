b9abf39b890acbe0b9406d15cca94e9378add06c
Anshul Verma <167604211+Coder-0120@users.noreply.github.com>
2025-06-19 15:49:24 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://abes-findify-backend.onrender.com',
+  baseURL: 'https://abes-findify-backend.onrender.com'|| "http://localhost:3000",
   withCredentials: true, // ✅ THIS IS IMPORTANT for sending cookies on login
 });
 
