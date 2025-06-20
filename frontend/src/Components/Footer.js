c9abf23ab853b39f25075c3986faed6b371f29df
Anshul Verma <167604211+Coder-0120@users.noreply.github.com>
2025-06-20 22:47:25 +0530

    Update Footer.js

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import {  Link } from 'react-router-dom';
+
 import '../Styles/Footer.css'; // Adjust the path as necessary
 const Footer = () => {
   return (
     <footer className="footer">
       <p>© 2025 ABES Findify. All rights reserved.</p>
       <div className="footer-links">
-        <a href="#features">Features</a>
-        <a href="/login">Login</a>
-        <a href="/register">Register</a>
+        <Link to="#features">Features</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </div>
     </footer>
   );
